6e674c2b64a5900b3a6136b6499d1e783415584d
Dima Konovko <dima.konovko@mail.ru>
2020-02-21 00:45:31 +0300

    add: balls generation and drawing

--- BallsAndCollisions/BallsAndCollisions/Source.cpp
+++ BallsAndCollisions/BallsAndCollisions/Source.cpp
@@ -7,19 +7,61 @@
 #include "SFML/Window.hpp"
 #include "SFML/System.hpp"
 
+//--------------------------------------------------
+//				Constants
+//
+
 const unsigned winWidth = 1280;
 const unsigned winHeight = 720;
 
+const unsigned char minBallsCount = 50;
+const unsigned char maxBallsCount = 70;
+
+const unsigned char minBallRadius = 10;
+const unsigned char maxBallRadius = 50;
+
+struct Ball
+{
+	float x;
+	float y;
+	float radius;
+
+	Ball(float x, float y, float radius) : x(x), y(y), radius(radius) { }
+};
+
+//--------------------------------------------------
+//				Global variables
+//
+
+std::unique_ptr<sf::RenderWindow> renderWindow;
+
+std::vector<Ball> balls;
+
+
+
+//--------------------------------------------------
+//				Forward declarations
+//
+
+void generateBalls();
+void drawBalls();
+
+
+
+//--------------------------------------------------
+//				Implementation
+//
+
 int main()
 {
-	std::srand(unsigned(std::time(0)));
-	
-	sf::RenderWindow renderWindow(sf::VideoMode(winWidth, winHeight), "BallsAndCollisions", sf::Style::Titlebar | sf::Style::Close);
+	generateBalls();
+
+	renderWindow = std::make_unique<sf::RenderWindow>(sf::VideoMode(winWidth, winHeight), "BallsAndCollisions", sf::Style::Titlebar | sf::Style::Close);
 	sf::Event event;
 
-	while (renderWindow.isOpen())
+	while (renderWindow->isOpen())
 	{
-		while (renderWindow.pollEvent(event))
+		while (renderWindow->pollEvent(event))
 		{
 			switch (event.type)
 			{
@@ -27,22 +69,57 @@ int main()
 				switch (event.key.code) 
 				{
 				case sf::Keyboard::Escape:
-					renderWindow.close();
+					renderWindow->close();
 					break;
 				}
 			case sf::Event::Closed:
-				renderWindow.close();
+				renderWindow->close();
 				break;
 			}
 		}
 
-		renderWindow.clear();
+		renderWindow->clear();
 
+		drawBalls();
 
-		renderWindow.display();
+		renderWindow->display();
 	}
 	
 
 
 	return 0;
+}
+
+
+
+void generateBalls()
+{
+	std::srand(unsigned(std::time(0)));
+
+	for (size_t ballN = 0; ballN < minBallsCount + rand() % maxBallsCount; ballN++)
+	{
+		balls.emplace_back(
+			Ball(
+				rand() % winWidth,
+				rand() % winHeight,
+				minBallRadius + rand() % maxBallRadius
+			)
+		);
+	}
+}
+
+
+
+void drawBalls()
+{
+	sf::CircleShape cs;
+	cs.setFillColor(sf::Color::Black);
+	cs.setOutlineColor(sf::Color::White);
+	cs.setOutlineThickness(2);
+	for (auto& ball : balls)
+	{
+		cs.setPosition(ball.x, ball.y);
+		cs.setRadius(ball.radius);
+		renderWindow->draw(cs);
+	}
 }
